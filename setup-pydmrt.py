907b98d73bc27321ba9af2a4d244973b7bd133ce
Florian Bruenig <florian.bruenig@fu-berlin.de>
2017-03-03 18:38:11 +0100

    add setup python script

---
+++ setup-pydmrt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+import numpy as np
+
+ext_modules = [ Extension('pydmrt',
+                extra_compile_args=['-fopenmp',"-I", "./","-I","../dmrt/dmrt/"],
+                extra_link_args=['-lgomp'],
+                sources = ['./dmrt/pydmrt.cpp', '../dmrt/dmrt/dmrtreader.cpp', '../dmrt/dmrt/dmrtalg2.cpp',
+                           '../dmrt/dmrt/dmrtmain.cpp']) ]
+
+setup(
+        name = 'pydmrt',
+        version = '0.1',
+        include_dirs = [np.get_include()], #Add Include path of numpy
+        ext_modules = ext_modules
+      )
