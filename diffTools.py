748bf4e080fbb363d3addc36d668740713e1de95
Florian Bruenig <florian.bruenig@fu-berlin.de>
2017-03-22 12:19:40 +0100

    refactor package to depend on unitools and be independent of diffProf, wrap all functionality in diffTools.py

--- diffTools.py
+++ diffTools.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
 from __future__ import print_function,division
-import diffProf2 as dp
-import diffProf_functions as dpf
+import diffTools_func as dtf
+import unitools as uni
 import numpy as np
 from scipy import integrate
 from os import listdir
 from os.path import isfile, join
 
+import pydmrt as pydmrt_module
+
+def pydmrt(inputVec, start=-2.0, interval=0.1, end=2.0, mode="rtcross", verb=False):
+    dmrtTms, dmrtCts, dmrtUpts = pydmrt_module.dmrtInp(inputVec, start, interval, end, mode,verb)
+    return dmrtTms, dmrtCts, dmrtUpts
+
 def loadEvalTxt(folder,filestring,mode,optionalstring="",verbose=0, recompute = False, rtCorrect=True):
 	finalTms=[]
 	finalCts=[]
-	if dpf.checkForPkl(folder,optionalstring+"_tms")==True and recompute==False:
-		tms =   dpf.openTemp(folder+filestring+mode+"_"+optionalstring+"_tms.pkl")
-		cts =   dpf.openTemp(folder+filestring+mode+"_"+optionalstring+"_cts.pkl")
-		dists = dpf.openTemp(folder+filestring+mode+"_"+optionalstring+"_dists.pkl")
+	if uni.checkForPkl(folder,optionalstring+"_tms")==True and recompute==False:
+		tms =   uni.openTemp(folder+filestring+mode+"_"+optionalstring+"_tms.pkl")
+		cts =   uni.openTemp(folder+filestring+mode+"_"+optionalstring+"_cts.pkl")
+		dists = uni.openTemp(folder+filestring+mode+"_"+optionalstring+"_dists.pkl")
 		return tms,cts,dists
 	for f in listdir(folder):
 		if f.startswith(filestring) and mode in f and optionalstring in f and "cts" in f and not ".pkl" in f:
@@ -39,16 +45,16 @@ def loadEvalTxt(folder,filestring,mode,optionalstring="",verbose=0, recompute = 
 	if "rt" in mode and rtCorrect:
 		tms = tms.T + tms
 		tms = +np.tril(tms)-np.triu(tms)
-	dpf.safeTemp(tms,folder+filestring+mode+"_"+optionalstring+"_tms.pkl")
-	dpf.safeTemp(cts,folder+filestring+mode+"_"+optionalstring+"_cts.pkl")
-	dpf.safeTemp(dists,folder+filestring+mode+"_"+optionalstring+"_dists.pkl")
+	uni.safeTemp(tms,folder+filestring+mode+"_"+optionalstring+"_tms.pkl")
+	uni.safeTemp(cts,folder+filestring+mode+"_"+optionalstring+"_cts.pkl")
+	uni.safeTemp(dists,folder+filestring+mode+"_"+optionalstring+"_dists.pkl")
 	return tms,cts,dists
 
 
 def getSolRdfFromTxt(folder,filestring,optionalstring, mfpt="mftp",cross="cross",verbose=0, recompute=False):
 		finalRdf=[]
-		if dpf.checkForPkl(folder,optionalstring+"_hist")==True and recompute==False:
-			rdf =   dpf.openTemp(folder+filestring+"_"+optionalstring+"_hist.pkl")
+		if uni.checkForPkl(folder,optionalstring+"_hist")==True and recompute==False:
+			rdf =   uni.openTemp(folder+filestring+"_"+optionalstring+"_hist.pkl")
 			return rdf
 
 		for f in listdir(folder):
@@ -62,14 +68,14 @@ def getSolRdfFromTxt(folder,filestring,optionalstring, mfpt="mftp",cross="cross"
 			rdf[:,1]=np.add(rdf[:,1],r[:,1])
 		if cross=="cross":
 			rdf[:-1,0]=(rdf[1:,0]+rdf[:-1,0])/2
-		dpf.safeTemp(rdf,folder+filestring+"_"+optionalstring+"_hist.pkl")
+		uni.safeTemp(rdf,folder+filestring+"_"+optionalstring+"_hist.pkl")
 		return rdf
 
 
 def fullAnalysis(dmrtMat, countsMat,dists, rdf, rmin = -1.5, rmax= 1.5,minVal = 0.2, maxVal = 1.0,mfpt="mftp",cross="cross", dim=1, verbose=0, smoothwidth=0):
     downsampleD=1
     print("Resolution: ", countsMat.shape,"Max counts: ", countsMat.max())
-    dmrtMat,dists = dp.filterDmrtMat(dmrtMat,dists ,rmin,rmax)
+    dmrtMat,dists = dtf.filterDmrtMat(dmrtMat,dists ,rmin,rmax)
     dmrtMat=dmrtMat[::downsampleD,::downsampleD]
     dists=dists[::downsampleD]
     if np.isnan(dmrtMat[:,-1]).any():
@@ -77,12 +83,12 @@ def fullAnalysis(dmrtMat, countsMat,dists, rdf, rmin = -1.5, rmax= 1.5,minVal = 
     if mfpt=="mftp":
         dmrtMat = -dmrtMat
     if smoothwidth==0:
-        ddmrt,ddists = dp.diffDmrtMat(dmrtMat,dists)
+        ddmrt,ddists = dtf.diffDmrtMat(dmrtMat,dists)
     else:
-        ddmrt,ddists = dp.diffDmrtMatYann(dmrtMat,dists,smoothwidth)
+        ddmrt,ddists = dtf.diffDmrtMatYann(dmrtMat,dists,smoothwidth)
     ddmrt = ddmrt[:,:-1]
     np.fill_diagonal(ddmrt,0.0)
-    ddmrt[ddmrt == 0] = dp.epsilon
+    ddmrt[ddmrt == 0] = uni.epsilon
 
     rdf[:,1]=rdf[:,1]/np.sum(rdf[:,1])
 
@@ -101,10 +107,11 @@ def fullAnalysis(dmrtMat, countsMat,dists, rdf, rmin = -1.5, rmax= 1.5,minVal = 
     diff = np.einsum('ij,i->ij', ddmrt, 1/interp)
     invdiff = 1/diff
 
-    filtMat = dp.filterMatrix(minVal,maxVal,dists)
+    filtMat = dtf.filterMatrix(minVal,maxVal,dists)
 
-    finalDmrts = dp.averageSelFilter(diff,filtMat[1:,1:])
+    finalDmrts = dtf.averageSelFilter(diff,filtMat[1:,1:])
 
     final =1/finalDmrts
 
     return final,ddists, ddmrt, diff
+
