2bd4f29869d22dfa61105985cb4b91ed436d6231
flo <flo@ju.physik.fu-berlin.de>
2017-06-06 17:06:55 +0200

    remove openMP from setup-pydmrt.py

--- setup-pydmrt.py
+++ setup-pydmrt.py
@@ -4,8 +4,7 @@ from distutils.core import setup, Extension
 import numpy as np
 
 ext_modules = [ Extension('pydmrt',
-                extra_compile_args=['-std=c++11','-fopenmp',"-Wno-sign-compare","-Wno-unused-variable","-Wno-unused-function","-Wno-unused-parameter","-I", "./","-I","../dmrt/dmrt/"],
-                extra_link_args=['-lgomp'],
+                extra_compile_args=['-std=c++11',"-Wno-sign-compare","-Wno-unused-variable","-Wno-unused-function","-Wno-unused-parameter","-I", "./","-I","../dmrt/dmrt/"],
                 sources = ['./dmrt/pydmrt.cpp', '../dmrt/dmrt/dmrtreader.cpp', '../dmrt/dmrt/dmrtalg2.cpp',
                            '../dmrt/dmrt/dmrtmain.cpp']) ]
 
